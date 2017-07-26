d332582e5b9bbf449934d2028991eebe18045f57
Lyubomir Vezev <l.vezev@gmail.com>
2017-07-26 16:13:11 +0300

    Hash user passwords before saving

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
@@ -46,7 +47,7 @@ UserSchema.methods.generateAuthToken = function () {
   var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
   user.tokens.push({access, token});
-  user.save().then(() => {
+  return user.save().then(() => {
     return token;
   });
 };
@@ -67,6 +68,21 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   });
 };
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  if ( user.isModified('password') ) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model('User', UserSchema);
 
